a40750526630e84424fb9d7cf6d77f500ebd5d5d
nikhilkuyya <nikhilkuyya@gmail.com>
2026-02-11 18:04:44 +0530

    chore: base setup

--- lexographical/return-value-of-string.js
+++ lexographical/return-value-of-string.js
@@ -1,17 +1,71 @@
 
-function returnValueOfString(str) {
+export function returnValueOfString(word) {
     const startCodeValue = 'a'.charCodeAt(0) - 1;
-    const indexBasedStringValue = str
+
+    const wordValues = word
         .split('')
-        .map(s => s.charCodeAt(0) - startCodeValue)
-        .map((val, index, arr) => {
+        .map(character => character.charCodeAt(0) - startCodeValue);
+
+    const positionBasedWordValue = wordValues
+        .map((character, index, arr) => {
             const length = arr.length;
-            return val * Math.pow(10, length - 1 - index);
+            return character * Math.pow(10, length - 1 - index);
         });
-    const result = indexBasedStringValue
+
+    const value = positionBasedWordValue
         .reduce((acc, v) => acc + v, 0);
+
+    return {
+        wordValues,
+        positionBasedWordValue,
+        wordValue: value
+    };
+}
+
+export function valueToStr(word){
+    const startCodeValue = 'a'.charCodeAt(0) - 1;
+    const wordValues = word
+    .map(character => character + startCodeValue)
+    .map(character => String.fromCharCode(character));
+return wordValues.join('')
+}
+
+export function testSwappable(wordValues) {
+    let isSwappable = false;
+    const wordLength = wordValues.length;
+    for (let it = wordLength - 1; it > 0; it -= 1) {
+        const currentCharValue = wordValues[it];
+        const previousCharValue = wordValues[it - 1];
+        if (currentCharValue > previousCharValue) {
+            isSwappable = true;
+            break;
+        }
+    }
+    return isSwappable;
+}
+
+export function sortAscendingOrder(wordCharCodes) {
+    let isSwapped = false;
+    let size = wordCharCodes.length;
+    let result = [...wordCharCodes];
+    do {
+        isSwapped = false;
+        let temp = [...result];
+        for (let it = 1; it < size; it += 1) {
+            const comparsion = temp[it - 1];
+            const comparator = temp[it];
+            if(comparsion > comparator) {
+                temp[it - 1] = comparator;
+                temp[it] =  comparsion;
+                isSwapped = true;
+            }
+        }
+        result = [...temp];
+    }while(isSwapped);
     return result;
 }
 
-console.log(returnValueOfString('ab'));
-console.log(returnValueOfString('ba'));
+export function basicSortAscendingOrder(wordCharCodes) {
+    return wordCharCodes.map(x => x).sort((a, b) => a - b);
+}
+
