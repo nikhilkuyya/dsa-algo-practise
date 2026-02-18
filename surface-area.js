c6d024d16aff6e85788815f0c9a8785c9b99ca86
nikhilkuyya <nikhilkuyya@gmail.com>
2026-02-18 09:17:54 +0530

    feat:compute surface area

--- surface-area.js
+++ surface-area.js
@@ -1,7 +1,6 @@
 import assert from 'node:assert';
 
 function surfaceArea(arr) {
-
     const cellFrontAreaAccumulation = arr.map((row, rowIndex) => {
         const rowAreaAccumulation = row.map((cell, cellIndex, currentRow) => {
             let currentCellFrontAreaAccumulation = 0;
@@ -13,17 +12,39 @@ function surfaceArea(arr) {
             }
             return currentCellFrontAreaAccumulation;
         });
-        return rowAreaAccumulation;        
+        return rowAreaAccumulation;
     });
 
-    const frontSurfaceArea = cellFrontAreaAccumulation.reduce((frontAreaAcc, row) => {
-        return frontAreaAcc + row.reduce((rowFrontAreaAcc, cell) => {
+    const frontSurfaceArea = computeMatrix(cellFrontAreaAccumulation);
+
+    const topFrontAreaAccumulation = arr.map((row) => {
+        const rowAreaAccumulation = row.map((cell) => {
+            const currentCellFrontAreaAccumulation = cell > 0 ? 1 : 0;
+            return currentCellFrontAreaAccumulation;
+        })
+        return rowAreaAccumulation
+    });
+
+    const topSurfaceArea = computeMatrix(topFrontAreaAccumulation);
+
+    const lateralAreaAccumulation = arr.map((row, rowIndex, mat) => {
+        if(rowIndex === 0) {return row;}
+        const previousRow = mat[rowIndex - 1];
+        return row.map((cell, cellIndex) => {
+            return cell > previousRow[cellIndex] ? cell - previousRow[cellIndex] : 0;            
+        });        
+    });
+
+    const lateralSurfaceArea = computeMatrix(lateralAreaAccumulation);
+    return (2 * frontSurfaceArea  + (2 * topSurfaceArea) + (2 * lateralSurfaceArea));
+}
+
+function computeMatrix(mat) {
+    return mat.reduce((acc, row) => {
+        return acc + row.reduce((rowFrontAreaAcc, cell) => {
             return rowFrontAreaAcc + cell;
         }, 0);
     }, 0);
-    
-    console.log({ cellFrontAreaAccumulation, frontSurfaceArea });
-    return 0;
 }
 
 
@@ -41,6 +62,7 @@ inputs.forEach((input, index) => {
     const output = surfaceArea(input);
     const expectedOutput = expected[index];
     try {
+        console.log({ output, expectedOutput });
         assert.ok(output === expectedOutput, `Test case ${index + 1} failed`);
     } catch (err) {
 
